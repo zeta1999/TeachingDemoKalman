6c9a081aaa4c4bd01c025720baba736c4179a763
Renoir Fortytwo <renaudb1999@gmail.com>
2019-06-03 11:23:46 +0000

    test

--- KalmanDemo.h
+++ KalmanDemo.h
@@ -1,1 +1,48 @@
+/*
+ * Small standalone Kalman demo.
+ * Standalone = you can use it /easily/ on your ESP32 or favourite embedded platform if you want...
+ */
+#pragma once
+#include <initializer_list>
+#include <vector>
 
+namespace Renoir42 {
+    template<typename T>
+    class Vector {
+        std::vector<T> data;
+        T *_data = { nullptr };
+    public:
+        inline Vector() = default;
+        inline Vector(const std::initializer_list<T> &i) {
+            data.assign(i);
+            _data = &data[0];
+        }
+
+        inline void resize(size_t s) {
+            data.resize(s);
+            _data = &data[0];
+        }
+
+        inline T* get() {
+            return _data;
+        }
+        inline const T* get() const {
+            return _data;
+        }
+        inline size_t size() const {
+            return data.size();
+        }
+    };
+
+    template<typename T>
+    class Matrix {
+        
+
+        // Cholesky
+        //
+    };
+
+    class Kalman {
+
+    };
+}
---
+++ Math.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace KalmanDemo {
+    // Inverse Cumulative Normal
+
+    
+}
